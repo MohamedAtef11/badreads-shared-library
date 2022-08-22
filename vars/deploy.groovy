e067dbed420b94baea008481b09fe67797a1599c
MohamedAtef11 <mohamed.atef023@outlook.com>
2022-08-22 17:53:43 +0200

    edit

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,6 +1,8 @@
 def call(){
+    withCredentials([file(credentialsId: 'kube-config', variable: 'Secretfile')]) {
     sh '''
         helm upgrade badreadsclient-$BRANCH_NAME --install ./badreads --values ./badreads/values-$BRANCH_NAME.yaml --set backend.image.repository=muhammadatef/badreadsserver-$BRANCH_NAME-$BUILD_NUMBER --set frontend.image.repository=muhammadatef/badreadsclient-$BRANCH_NAME-$BUILD_NUMBER --kubeconfig $Secretfile
         '''
     cleanWs()
+    }
 }
