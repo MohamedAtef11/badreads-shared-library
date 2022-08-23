4179b3dcb0c72d419c68c39e9ee9aa54e9738826
MohamedAtef11 <mohamed.atef023@outlook.com>
2022-08-23 18:59:27 +0200

    add resources

--- vars/loaddockerfiles.groovy
+++ vars/loaddockerfiles.groovy
@@ -9,7 +9,7 @@ def call(){
                 writeFile file: "badreads/${file_name}", text: content
             }
     def dir_templates= "badreads/templates/back-end-deployment.yaml badreads/templates/backend-service.yaml badreads/templates/front-end-deployment.yaml badreads/templates/frontend-service.yaml"
-    def dirs = dir_templates.split(" ")
+    def dirs_templates = dir_templates.split(" ")
         for (i in dirs) {
                 def file_name = i.split("/")[-1]
                 def content = libraryResource i
