2e5964c5b48ed3fc6340e0006667069ebdb0d85e
MohamedAtef11 <mohamed.atef023@outlook.com>
2022-08-24 00:43:14 +0200

    add resources

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,5 +1,5 @@
 def call(){
-    loaddockerfiles()
+    loadhelmfiles()
     withCredentials([file(credentialsId: 'kube-config', variable: 'Secretfile')]) {
     sh '''
         helm upgrade badreadsclient-$BRANCH_NAME --install ./badreads --values ./badreads/values-$BRANCH_NAME.yaml --set backend.image.repository=muhammadatef/badreadsserver-$BRANCH_NAME-$BUILD_NUMBER --set frontend.image.repository=muhammadatef/badreadsclient-$BRANCH_NAME-$BUILD_NUMBER --kubeconfig $Secretfile
