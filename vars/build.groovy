274888eae556e6aaa0b3e5b44fe08c8fe15e6f62
MohamedAtef11 <mohamed.atef023@outlook.com>
2022-08-23 18:03:40 +0200

    add resources

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,13 +1,13 @@
 def call() {
 
-    loaddockerfiles()
     withCredentials([usernamePassword(credentialsId: 'docker_cred', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
         sh """
             docker login -u '${USERNAME}' -p '${PASSWORD}'
-            docker build -t muhammadatef/badreadsclient-$BRANCH_NAME-$BUILD_NUMBER -f ./Dockerfile-front
-            docker build -t muhammadatef/badreadsserver-$BRANCH_NAME-$BUILD_NUMBER -f ./Dockerfile-back
+            docker build -t muhammadatef/badreadsclient-$BRANCH_NAME-$BUILD_NUMBER ./badreads-frontend
+            docker build -t muhammadatef/badreadsserver-$BRANCH_NAME-$BUILD_NUMBER ./badreads-backend 
             docker push muhammadatef/badreadsclient-$BRANCH_NAME-$BUILD_NUMBER 
             docker push muhammadatef/badreadsserver-$BRANCH_NAME-$BUILD_NUMBER 
+
             """
         }
 
--- vars/loaddockerfiles.groovy
+++ vars/loaddockerfiles.groovy
@@ -1,8 +1,12 @@
 def call(){
 
-    def dockerclient = libraryResource "badreads-frontend/Dockerfile"
-    writeFile file: "Dockerfile-front" , text: dockerclient
-    def dockerserver = libraryResource "badreads-backend/Dockerfile"
-    writeFile file: "Dockerfile-back" , text: dockerserver
+    sh "mkdir -p badreads/templates"
+    def all_resource= "badreads/templates/back-end-deployment.yaml badreads/templates/backend-service.yaml badreads/templates/front-end-deployment.yaml badreads/templates/frontend-service.yaml"
+    def dirs = all_resource_dir.split(" ")
+        for (i in dirs) {
+                def file_name = i.split("/")[-1]
+                def content = libraryResource i
+                writeFile file: "badreads/templates/${file_name}", text: content
+            }
 
 }
