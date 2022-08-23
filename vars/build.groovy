e93984284bdc35f5b61ca2bc066d5af1ffadf8c0
MohamedAtef11 <mohamed.atef023@outlook.com>
2022-08-23 17:26:47 +0200

    add resources

--- vars/build.groovy
+++ vars/build.groovy
@@ -4,8 +4,8 @@ def call() {
     withCredentials([usernamePassword(credentialsId: 'docker_cred', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
         sh """
             docker login -u '${USERNAME}' -p '${PASSWORD}'
-            docker build -t muhammadatef/badreadsclient-$BRANCH_NAME-$BUILD_NUMBER ./Dockerfile-front
-            docker build -t muhammadatef/badreadsserver-$BRANCH_NAME-$BUILD_NUMBER ./Dockerfile-back
+            docker build -t muhammadatef/badreadsclient-$BRANCH_NAME-$BUILD_NUMBER -f ./Dockerfile-front
+            docker build -t muhammadatef/badreadsserver-$BRANCH_NAME-$BUILD_NUMBER -f ./Dockerfile-back
             docker push muhammadatef/badreadsclient-$BRANCH_NAME-$BUILD_NUMBER 
             docker push muhammadatef/badreadsserver-$BRANCH_NAME-$BUILD_NUMBER 
             """
